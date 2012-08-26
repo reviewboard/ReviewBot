9dbe4e49407a4088673b039e5b8b8b1d13adc265
Steven MacLeod <steven@smacleod.ca>
2012-08-26 18:27:11 -0400

    added logging to the worker task

--- bot/reviewbot/tasks.py
+++ bot/reviewbot/tasks.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 import json
+import logging
 import pkg_resources
 
+from celery.utils.log import get_task_logger
 from celery.worker.control import Panel
 from reviewbot.celery import celery
 from rbtools.api.client import RBClient
@@ -13,11 +15,21 @@ COOKIE_FILE = 'reviewbot-cookies.txt'
 # TODO: Include version information in the agent.
 AGENT = 'ReviewBot'
 
+logger = get_task_logger("WORKER")
+
 
 @celery.task(ignore_result=True)
 def ProcessReviewRequest(payload, tool_settings):
     """Execute an automated review on a review request."""
+    routing_key = ProcessReviewRequest.request.delivery_info['routing_key']
+    route_parts = routing_key.partition('.')
+    tool_ep = route_parts[0]
+    logger.info(
+        "Request to execute review tool '%s' for %s" % (
+            tool_ep, payload['url']))
+
     try:
+        logger.info("Initializing RB API")
         api_client = RBClient(
             payload['url'] + 'api/',
             cookie_file=COOKIE_FILE,
@@ -26,32 +38,54 @@ def ProcessReviewRequest(payload, tool_settings):
             agent='ReviewBot')
         api_root = api_client.get_root()
     except:
+        logger.error("Could not contact RB server at '%s'" % payload['url'])
         return False
 
-    routing_key = ProcessReviewRequest.request.delivery_info['routing_key']
-    ep_name = routing_key.partition('.')[0]
+    logger.info("Loading requested tool '%s'" % tool_ep)
     tools = []
     for ep in pkg_resources.iter_entry_points(group='reviewbot.tools',
-                                              name=ep_name):
+                                              name=tool_ep):
         tools.append(ep.load())
 
-    if len(tools) != 1:
-        # There was either no Tool, or too many tools.
-        # TODO: Properly report the errors.
+    if len(tools) > 1:
+        logger.error("Tool '%s' is ambiguous" % tool_ep)
+        return False
+    elif len(tools) == 0:
+        logger.error("Tool '%s' not found" % tool_ep)
         return False
 
     tool = tools[0]
     try:
+        logger.info("Initializing review")
         review = Review(api_root, payload['request'], payload['settings'])
-        t = tool(review, settings=tool_settings)
-        t.execute()
-        review.publish()
-    except:
-        # Something went wrong with this tool.
-        # TODO: Stop ignoring errors and do something
-        # about them.
+    except Exception, e:
+        logger.error("Error initializing review: %s" % str(e))
         return False
 
+    try:
+        logger.info("Initializing tool '%s' version '%s'" %
+            (tool.name, tool.version))
+        t = tool(review, settings=tool_settings)
+    except Exception, e:
+        logger.error("Error initializing tool: %s" % str(e))
+        return False
+
+    try:
+        logger.info("Executing tool '%s'" % t.name)
+        t.execute()
+        logger.info("Tool execution completed successfully")
+    except Exception, e:
+        logger.error("Error executing tool: %s" % str(e))
+        return False
+
+    try:
+        logger.info("Publishing review")
+        review.publish()
+    except Exception, e:
+        logger.error("Error publishing review: %s" % str(e))
+        return False
+
+    logger.info("Review completed successfully")
     return True
 
 
