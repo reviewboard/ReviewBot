a2fe7f92399ab1c0a8ae457759bee04864c236b0
Steven MacLeod <steven@smacleod.ca>
2012-09-18 19:55:37 -0400

    Fixed up a few style issues.

--- bot/setup.py
+++ bot/setup.py
@@ -4,22 +4,22 @@ PACKAGE_NAME = "ReviewBot"
 VERSION = "0.1"
 
 
-setup(name=PACKAGE_NAME,
-      version=VERSION,
-      description="ReviewBot, the automated code reviewer",
-      author="Steven MacLeod",
-      author_email="steven@smacleod.ca",
-      packages=find_packages(),
-      entry_points={
-          'console_scripts': [
-              'reviewbot = reviewbot.celery:main'
-          ],
-          'reviewbot.tools': [
-              'pep8 = reviewbot.tools.pep8:pep8Tool',
-          ],
-      },
-      install_requires=[
-          'celery>=3.0',
-          'pep8>=0.7.0',
-      ],
-)
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description="ReviewBot, the automated code reviewer",
+    author="Steven MacLeod",
+    author_email="steven@smacleod.ca",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'reviewbot = reviewbot.celery:main'
+        ],
+        'reviewbot.tools': [
+            'pep8 = reviewbot.tools.pep8:pep8Tool',
+        ],
+    },
+    install_requires=[
+        'celery>=3.0',
+        'pep8>=0.7.0',
+    ],)
--- extension/reviewbotext/admin_urls.py
+++ extension/reviewbotext/admin_urls.py
@@ -4,9 +4,12 @@ from reviewbotext.extension import ReviewBotExtension
 from reviewbotext.forms import ReviewBotSettingsForm
 
 
-urlpatterns = patterns('',
-    (r'^$', 'reviewboard.extensions.views.configure_extension',
-     {'ext_class': ReviewBotExtension,
-      'form_class': ReviewBotSettingsForm,
-    }),
-)
+urlpatterns = patterns(
+    '',
+    (
+        r'^$', 'reviewboard.extensions.views.configure_extension',
+        {
+            'ext_class': ReviewBotExtension,
+            'form_class': ReviewBotSettingsForm,
+        }
+    ))
