c8e4cf80cb1023d64fae661ad92a780eb3d1db0c
Steven MacLeod <steven@smacleod.ca>
2014-06-10 10:40:48 -0400

    Start updating the extensions Web API.
    The new DB structure and features requires all new API resources. This
    tears out some of the old stuff and starts writing the new resources.
    A lot of the old code will be reused down the road but it was just
    easier to start fresh like this and the old code can be pulled out of
    the repo later.

--- extension/reviewbotext/resources.py
+++ extension/reviewbotext/resources.py
@@ -1,179 +1,34 @@
 import json
 
-from django.core.exceptions import ObjectDoesNotExist
+from djblets.webapi.decorators import (webapi_login_required,
+                                       webapi_response_errors,
+                                       webapi_request_fields)
+from djblets.webapi.errors import (DOES_NOT_EXIST,
+                                   INVALID_FORM_DATA,
+                                   NOT_LOGGED_IN,
+                                   PERMISSION_DENIED)
 
-from djblets.webapi.decorators import webapi_login_required, \
-                                      webapi_response_errors, \
-                                      webapi_request_fields
-from djblets.webapi.errors import DOES_NOT_EXIST, INVALID_FORM_DATA, \
-                                  NOT_LOGGED_IN, PERMISSION_DENIED
-
-from reviewboard.diffviewer.models import FileDiff
 from reviewboard.extensions.base import get_extension_manager
-from reviewboard.reviews.models import BaseComment, Review, ReviewRequest
 from reviewboard.webapi.decorators import webapi_check_local_site
-from reviewboard.webapi.resources import WebAPIResource, resources
+from reviewboard.webapi.resources import WebAPIResource
 
-from reviewbotext.models import ReviewBotTool
-
-
-class ReviewBotReviewResource(WebAPIResource):
-    """Resource for creating reviews with a single request.
-
-    This resource allows Review Bot to create a full review using a single
-    POST request. Using the traditional API would result in a high volume
-    of requests from Review Bot, creating stress on the server.
-
-    Each user may only have one review draft per request at a time. This
-    resource allows concurrent review of a single request by creating
-    the review and publishing it in a single transaction.
-    """
-    name = 'review_bot_review'
-    allowed_methods = ('GET', 'POST',)
-
-    @webapi_check_local_site
-    @webapi_login_required
-    @webapi_response_errors(DOES_NOT_EXIST, INVALID_FORM_DATA,
-                            NOT_LOGGED_IN, PERMISSION_DENIED)
-    @webapi_request_fields(
-        required={
-            'review_request_id': {
-                'type': int,
-                'description': 'The ID of the review request.'
-            },
-        },
-        optional={
-            'ship_it': {
-                'type': bool,
-                'description': 'Whether or not to mark the review "Ship It!"',
-            },
-            'body_top': {
-                'type': str,
-                'description': 'The review content above the comments.',
-            },
-            'body_bottom': {
-                'type': str,
-                'description': 'The review content below the comments.',
-            },
-            'diff_comments': {
-                'type': str,
-                'description': 'A JSON payload containing the diff comments',
-            },
-        },
-    )
-    def create(self, request, review_request_id, ship_it=False, body_top='',
-               body_bottom='', diff_comments=None, *args, **kwargs):
-        """Creates a new review and publishes it."""
-        try:
-            review_request = resources.review_request.get_object(
-                request,
-                review_request_id=review_request_id,
-                *args, **kwargs)
-        except ObjectDoesNotExist:
-            return DOES_NOT_EXIST
-
-        if not body_top:
-            body_top = ''
-
-        if not body_bottom:
-            body_bottom = ''
-
-        new_review = Review.objects.create(
-            review_request=review_request,
-            user=request.user,
-            body_top=body_top,
-            body_bottom=body_bottom,
-            ship_it=ship_it)
-
-        if diff_comments:
-            try:
-                diff_comments = json.loads(diff_comments)
-                for comment in diff_comments:
-                    filediff = FileDiff.objects.get(
-                        pk=comment['filediff_id'],
-                        diffset__history__review_request=review_request)
-
-                    if comment['issue_opened']:
-                        issue = True
-                        issue_status = BaseComment.OPEN
-                    else:
-                        issue = False
-                        issue_status = None
-
-                    new_review.comments.create(
-                        filediff=filediff,
-                        interfilediff=None,
-                        text=comment['text'],
-                        first_line=comment['first_line'],
-                        num_lines=comment['num_lines'],
-                        issue_opened=issue,
-                        issue_status=issue_status)
-
-            except KeyError:
-                # TODO: Reject the DB transaction.
-                return INVALID_FORM_DATA, {
-                    'fields': {
-                        'diff_comments': 'Diff comments were malformed',
-                    },
-                }
-            except ObjectDoesNotExist:
-                return INVALID_FORM_DATA, {
-                    'fields': {
-                        'diff_comments': 'Invalid filediff_id',
-                    },
-                }
-
-        new_review.publish(user=request.user)
-
-        return 201, {
-            self.item_result_key: new_review,
-        }
-
-review_bot_review_resource = ReviewBotReviewResource()
+from reviewbotext.models import Profile, Tool
 
 
-class ReviewBotToolResource(WebAPIResource):
+EXTENSION_MANAGER = get_extension_manager()
+
+
+class ToolResource(WebAPIResource):
     """Resource for workers to update the installed tools list.
 
-    Also provides information on the installed Review Bot tools. This can be
-    used to fetch the list of installed tools to be displayed in the Review
-    Board UI.
-
+    This API endpoint isn't actually RESTful, and just provides a place
+    for workers to "dump" their entire list of installed tools as a single
+    POST. A GET request will not actually return a list of tools.
     """
-    name = 'review_bot_tool'
+    name = 'tool'
     allowed_methods = ('GET', 'POST',)
-    model = ReviewBotTool
     model_object_key = 'id'
-    uri_object_key = 'reviewbot_tool_id'
-    fields = {
-        'id': {
-            'type': int,
-            'description': 'The id of the tool.',
-        },
-        'name': {
-            'type': str,
-            'description': 'The name of the tool.',
-        },
-        'version': {
-            'type': str,
-            'description': 'The tool version number.',
-        },
-        'enabled': {
-            'type': bool,
-            'description': 'Whether the user has enabled this '
-                           'tool in the Review Bot extension.',
-        },
-        'run_automatically': {
-            'type': bool,
-            'description': 'Whether the user has enabled this tool to '
-                           'run automatically.',
-        },
-        'allow_run_manually': {
-            'type': bool,
-            'description': 'Whether the user has enabled this tool to '
-                           'be triggered manually.',
-        },
-    }
+    uri_object_key = 'tool_id'
 
     @webapi_check_local_site
     @webapi_login_required
@@ -198,7 +53,7 @@ class ReviewBotToolResource(WebAPIResource):
         worker is using (This should be unique to that worker under
         proper configuration).
 
-        The tools file should contain a JSON payload describing the
+        The tools field should contain a JSON payload describing the
         list of tools installed at the worker. This payload should
         correspond to a list of dictionaries, with each dictionary
         corresponding to a tool. The dictionary should contain the
@@ -208,10 +63,36 @@ class ReviewBotToolResource(WebAPIResource):
             - 'version': The tool version.
             - 'description': Longer description of the tool.
             - 'tool_options': A JSON payload describing the custom
-              options the tool provides.
+              options the tool provides (see reviewbotext.models.Tool
+              for a description of this payload).
+
+        Here is an example tools payload:
+        [
+          {
+            "name": "Example Tool 1",
+            "entry_point": "example1",
+            "version": "1.0.1",
+            "description": "An example tool.",
+            "tool_options": "[]"
+          },
+          {
+            "name": "Example Tool 2",
+            "entry_point": "example2",
+            "version": "1.2.1",
+            "description": "The second example tool.",
+            "tool_options": "[]"
+          },
+        ]
 
         TODO: Use the hostname.
         """
+        from reviewbotext.extension import ReviewBotExtension
+        extension = EXTENSION_MANAGER.get_enabled_extension(ReviewBotExtension.id)
+
+        # Ensure the request is from the Review Bot user
+        if request.user.id != extension.settings['user']:
+            return PERMISSION_DENIED
+
         try:
             tools = json.loads(tools)
         except:
@@ -222,25 +103,16 @@ class ReviewBotToolResource(WebAPIResource):
                 }
 
         for tool in tools:
-            # TODO: Defaulting the settings should maybe
-            # occure when sending out payloads, not here.
-            # or it could be taken care of on the tool
-            # side.
-            options = json.loads(str(tool['tool_options']))
-            settings = {}
-            for opt in options:
-                settings[opt['name']] = opt.get('default', None)
-
-            obj, created = ReviewBotTool.objects.get_or_create(
+            obj, created = Tool.objects.get_or_create(
                 entry_point=tool['entry_point'],
                 version=tool['version'],
                 defaults={
                     'name': tool['name'],
                     'description': tool['description'],
                     'tool_options': tool['tool_options'],
-                    'tool_settings': json.dumps(settings),
                     'in_last_update': True,
                 })
+
             if not created and not obj.in_last_update:
                 obj.in_last_update = True
                 obj.save()
@@ -248,82 +120,28 @@ class ReviewBotToolResource(WebAPIResource):
         # TODO: Fix the result key here.
         return 201, {}
 
-review_bot_tool_resource = ReviewBotToolResource()
+tool_resource = ToolResource()
 
 
-class ReviewBotTriggerReviewResource(WebAPIResource):
-    """Resource that triggers a set of ReviewBot reviews given a list of tools
-    to run on a review request.
+# TODO: continue here
+# What's the best way to allow manual execution and querying of status
+# with resources. What endpoints do I need and how should they be filtered
+# etc. Will need the following things:
+#
+# - Execution manually from a profile ID and Diff ID
+# - Display status for a Diff ID + Review Request ID
+#   - What can a particular user ID run for this diff?
+#   - What IS currently running for this diff?
+#   - What IS currently running for ANY diffs?
 
-    Can be called from within the ReviewBoard UI for the purpose of manually
-    triggering ReviewBot reviews.
 
+class ProfileResource(WebAPIResource):
+    """Provides information about tool profiles and allows manual execution.
     """
-    name = 'review_bot_trigger_review'
-    allowed_methods = ('POST',)
+    name = 'profile'
+    allowed_methods = ('GET',)
+    model_object_key = 'id'
+    uri_object_key = 'profile'
 
-    @webapi_check_local_site
-    @webapi_login_required
-    @webapi_response_errors(DOES_NOT_EXIST, INVALID_FORM_DATA,
-                            NOT_LOGGED_IN, PERMISSION_DENIED)
-    @webapi_request_fields(
-        required={
-            'review_request_id': {
-                'type': int,
-                'description': 'ID of the review request to be reviewed.',
-            },
-            'tools': {
-                'type': str,
-                'description': 'A JSON payload containing tool information.',
-            },
-        },
-    )
-    def create(self, request, review_request_id, tools, *args, **kwargs):
-        """Trigger ReviewBot review(s) based on the set of given tools.
-
-        """
-        try:
-            tools = json.loads(tools)
-
-        except:
-            return INVALID_FORM_DATA, {
-                'fields': {
-                    'dtools': 'Malformed JSON.',
-                },
-            }
-
-        # Get the ReviewBotExtension instance. We can assume it exists because
-        # this code is executed after the extension has been registered with
-        # the manager.
-        from reviewbotext.extension import ReviewBotExtension
-        extension_manager = get_extension_manager()
-        extension = \
-            extension_manager.get_enabled_extension(ReviewBotExtension.id)
-        review_request = ReviewRequest.objects.get(id=review_request_id)
-        is_new = True
-        fields_changed = {}
-
-        # Check for a diff:
-        diffsets = review_request.diffset_history.diffsets.get_query_set()
-        if len(diffsets) > 0:
-            has_diff = True
-            diff_revision = diffsets[len(diffsets)-1].revision
-        else:
-            has_diff = False
-
-        request_payload = {
-            'review_request_id': review_request_id,
-            'new': is_new,
-            'fields_changed': fields_changed,
-            'has_diff': has_diff,
-        }
-
-        if has_diff:
-            request_payload['diff_revision'] = diff_revision
-
-        extension.notify(request_payload, tools)
-
-        # TODO: Fix the result key here.
-        return 201, {}
-
-review_bot_trigger_review_resource = ReviewBotTriggerReviewResource()
+    model = Profile
+    name = 'profile'
