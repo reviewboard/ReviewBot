372a0e5b912c409b724e1c4ae2d23d8fd8deea62
Christian Hammond <christian@beanbaginc.com>
2018-07-31 01:06:20 -0700

    Update the metadata for the Review Bot packages.
    This adds the new READMEs to the Review Bot extension and worker
    packages. It also updates the classifiers to promote the stability level
    to Production/Stable, add the Quality Assurance topic, and adds the
    Python versions.
    
    Testing Done:
    Built the packages and checked the resulting metadata for the README and
    the classifiers.
    
    Reviewed at https://reviews.reviewboard.org/r/10110/

--- extension/setup.py
+++ extension/setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
-from setuptools import find_packages
+
 from reviewboard.extensions.packaging import setup
+from setuptools import find_packages
 
 from reviewbotext import get_package_version
+
+
+with open('README.rst', 'r') as fp:
+    long_description = fp.read()
 
 
 setup(
@@ -11,6 +16,7 @@ setup(
     license='MIT',
     description=('Review Bot, the automated code reviewer (Review Board '
                  'extension)'),
+    long_description=long_description,
     author='Beanbag, Inc.',
     author_email='support@beanbaginc.com',
     maintainer='Beanbag, Inc.',
@@ -25,13 +31,16 @@ setup(
         'celery>=3.0,<4.0',
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Review Board',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development',
+        'Topic :: Software Development :: Quality Assurance',
     ],
 )
