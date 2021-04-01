d02596d37da0ffb88bd87739cc4571934814e936
Christian Hammond <christian@beanbaginc.com>
2021-03-31 17:54:44 -0700

    Release Review Bot 2.0.1.

--- docs/reviewbot/conf.py
+++ docs/reviewbot/conf.py
@@ -20,6 +20,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('_ext'))
 
+parent_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
+sys.path.insert(0, os.path.join(parent_dir, 'bot'))
+sys.path.insert(0, os.path.join(parent_dir, 'extension'))
+
 import reviewbotext
 
 # -- General configuration ------------------------------------------------
