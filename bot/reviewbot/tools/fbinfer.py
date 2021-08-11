262947e6a45799b2aa13f67c79824dfae6fd332c
Christian Hammond <christian@beanbaginc.com>
2021-08-10 22:36:55 -0700

    Update FBInfer for new tool support and improved parsing.
    This updates `FBInferTool` to subclass the newer `BaseTool`, and to use
    the newer features it offers (dependencies, improved file extension
    handling, centralized base command building, and new comment options).
    
    A lot of the code is now better organized, leveraging these new tool
    features. Report parsing is a bit more consistent now, with results
    being sorted by filename, line number, and then column number.
    
    Most of FBInfer is straight-forward, but when using the CMake build
    type, there is an extra "compile" step that generates a Makefile and
    switches the build type to "make".
    
    If working with a multi-file build type, the command will be run in
    `handle_files()`, and the resulting report loaded.
    
    If working with a single-file build type, `handle_file()` will take
    over, generating reports for each file, and combining all reports into a
    single report.
    
    In either case, the result is a report. `handle_files()` loops over
    that, adding comments to the report.
    
    Unit tests were added for single and multi-file builds and for the
    special CMake compilation step.
    
    Testing Done:
    All unit tests pass on Python 2 and 3.
    
    Reviewed at https://reviews.reviewboard.org/r/11773/

--- bot/reviewbot/tools/fbinfer.py
+++ bot/reviewbot/tools/fbinfer.py
@@ -3,17 +3,15 @@
 from __future__ import unicode_literals
 
 import json
-import logging
+import os
 import shlex
 
-from reviewbot.tools import RepositoryTool
-from reviewbot.utils.process import execute, is_exe_in_path
+from reviewbot.config import config
+from reviewbot.tools.base import BaseTool, FullRepositoryToolMixin
+from reviewbot.utils.process import execute
 
 
-logger = logging.getLogger(__name__)
-
-
-class FBInferTool(RepositoryTool):
+class FBInferTool(FullRepositoryToolMixin, BaseTool):
     """Review Bot tool to run FBInfer."""
 
     name = 'FBInfer'
@@ -21,24 +19,27 @@ class FBInferTool(RepositoryTool):
     description = ('Checks code for errors using FBInfer, a tool for static '
                    'Android, Java, C, C++, and iOS/Objective-C code analysis.')
     timeout = 90
+
+    exe_dependencies = ['infer']
+
     options = [
         {
             'name': 'build_type',
             'field_type': 'django.forms.ChoiceField',
             'field_options': {
-                'label': 'Select Build Type',
+                'label': 'Build system',
                 'help_text': 'Choose how the project will be compiled.',
                 'choices': (
-                    ('./gradlew', 'Android/Gradle with Wrapper'),
-                    ('ant', 'Apache Ant'),
-                    ('buck build', 'Buck'),
-                    ('clang -c', 'C/Objective-C'),
-                    ('cmake', 'CMake'),
-                    ('gradle', 'Gradle'),
-                    ('xcodebuild', 'iOS/XCode'),
-                    ('javac', 'Java'),
-                    ('make', 'Make'),
-                    ('mvn', 'Maven'),
+                    ('./gradlew', 'Android/Gradle with Wrapper (./gradlew)'),
+                    ('ant', 'Apache Ant (ant)'),
+                    ('buck build', 'Buck (buck build)'),
+                    ('clang -c', 'Clang (clang -c)'),
+                    ('cmake', 'CMake (cmake)'),
+                    ('gradle', 'Gradle (gradle)'),
+                    ('javac', 'Java (javac)'),
+                    ('make', 'Make (make)'),
+                    ('mvn', 'Maven (mvn)'),
+                    ('xcodebuild', 'XCode (xcodebuild)'),
                 ),
             },
         },
@@ -47,12 +48,11 @@ class FBInferTool(RepositoryTool):
             'field_type': 'django.forms.CharField',
             'default': '',
             'field_options': {
-                'label': 'Build Target',
+                'label': 'Build target',
                 'required': False,
                 'help_text': (
-                    'Include a build target if required to successfully build '
-                    'the project. (e.g. pass in the "build" parameter for a '
-                    'gradle compilation or a target for a buck compilation)'
+                    'The name of the target to build, if the build system '
+                    'needs one or is capable of building multiple targets.'
                 ),
             },
         },
@@ -61,10 +61,13 @@ class FBInferTool(RepositoryTool):
             'field_type': 'django.forms.CharField',
             'default': '',
             'field_options': {
-                'label': 'XCode Configuration',
+                'label': 'XCode configuration',
                 'required': False,
-                'help_text': ('Include any additional configuration options '
-                              'for the Xcode -configuration flag.'),
+                'help_text': (
+                    'Any additional configuration options needed for a '
+                    'XCode build (equivalent to the -configuration flag). '
+                    'This is ignored for non-XCode builds'
+                ),
             },
         },
         {
@@ -72,163 +75,253 @@ class FBInferTool(RepositoryTool):
             'field_type': 'django.forms.CharField',
             'default': '',
             'field_options': {
-                'label': 'SDK',
+                'label': 'XCode SDK',
                 'required': False,
-                'help_text': ('Include an SDK required to build a project '
-                              'such as "iphonesimulator" for iOS.'),
+                'help_text': (
+                    'Include an XCode SDK required to build a project '
+                    'such as "iphonesimulator" for iOS. This is ignored for '
+                    'non-XCode builds.'
+                )
             },
         },
     ]
 
     # Tuple of builds that do not require a path.
-    multi_file_build_types = (
+    MULTI_FILE_BUILD_TYPES = {
+        './gradlew',
         'ant',
         'buck build',
         'cmake',
         'gradle',
-        './gradlew',
         'make',
         'mvn',
         'xcodebuild',
-    )
+    }
 
-    def check_dependencies(self):
-        """Verify the tool's dependencies are installed.
+    def build_base_command(self, **kwargs):
+        """Build the base command line used to review files.
+
+        Args:
+            **kwargs (dict, unused):
+                Additional keyword arguments.
 
         Returns:
-            bool:
-            True if all dependencies for the tool are satisfied. If this
-            returns False, the worker will not listen for this Tool's queue,
-            and a warning will be logged.
+            list of unicode:
+            The base command line.
         """
-        return is_exe_in_path('infer')
+        settings = self.settings
+        build_type = settings['build_type']
+        build_target = settings.get('build_target', '').strip()
 
-    def handle_files(self, files, settings):
+        if build_type == 'cmake':
+            # CMake is treated specially. We'll be running `cmake` in
+            # _run_multi_file_build() using `fbinfer compile`, and then we'll
+            # switch to `make`. We're building that second-stage command, so
+            # change to `make` here.
+            build_type = 'make'
+
+        cmd = [
+            config['exe_paths']['infer'],
+            'run',
+            '--no-progress-bar',
+            '--',
+        ] + shlex.split(build_type)
+
+        if build_type == 'xcodebuild':
+            xcode_configuration = \
+                settings.get('xcode_configuration', '').strip()
+            xcode_sdk = settings.get('sdk', '').strip()
+
+            if build_target:
+                cmd += ['-target', build_target]
+
+            if xcode_configuration:
+                cmd += ['-configuration', xcode_configuration]
+
+            if xcode_sdk:
+                cmd += ['-sdk', xcode_sdk]
+        else:
+            if build_target:
+                cmd.append(build_target)
+
+        return cmd
+
+    def handle_files(self, files, review, base_command, **kwargs):
         """Perform a review of all files.
 
         Args:
             files (list of reviewbot.processing.review.File):
                 The files to process.
 
-            settings (dict):
-                Tool-specific settings.
+            review (reviewbot.processing.review.Review):
+                The review being populated.
+
+            base_command (list of unicode):
+                The base command used to run infer.
+
+            **kwargs (dict):
+                Additional keyword arguments.
         """
-        self.build_type = settings['build_type']
+        build_type = self.settings['build_type']
 
-        if self.build_type in self.multi_file_build_types:
-            self.run_multi_file_build(files, settings)
-
-            for f in files:
-                self.report_file(f)
-        else:
-            for f in files:
-                self.run_single_file_build(f)
-                self.report_file(f)
-
-    def report_file(self, f):
-        """Report infer results for a given file.
-
-        Args:
-            f (reviewbot.processing.review.File):
-                The file to process.
-        """
-        path = f.get_patched_file_path()
-
-        if not path:
-            return
-
-        try:
-            with open('infer-out/report.json', 'r') as fp:
-                json_data = json.load(fp)
-                for entry in json_data:
-                    if path == entry['file']:
-                        f.comment('Bug Type: %(bug_type_hum)s\n'
-                                  '%(severity)s %(qualifier)s'
-                                  % entry,
-                                  first_line=entry['line'],
-                                  rich_text=True)
-        except Exception as e:
-            logger.exception('JSON file could not be opened/loaded: %s', e)
-
-    def run_multi_file_build(self, files, settings):
-        """Perform execution of FBInfer run on a multi-file build
-
-        Args:
-            files (list of reviewbot.processing.review.File):
-                The files to process.
-
-            settings (dict):
-                Tool-specific settings.
-        """
-        # CMake requires a compilation before analysis.
-        if self.build_type == 'cmake':
+        if build_type == 'cmake':
+            # CMake has an extra compilation step before we can run
+            # `fbinfer run`. Do that here.
             try:
                 execute([
-                    'infer',
+                    config['exe_paths']['infer'],
                     'compile',
                     '--',
-                    self.build_type,
+                    build_type,
                     '.',
                 ])
-
-                # After compilation, update build_type from CMake to Make.
-                # FBInfer uses Make in `infer run` to do its analysis.
-                self.build_type = 'make'
             except Exception as e:
-                logger.exception('FBInfer compile command failed with build '
-                                 'type: %s %s', self.build_type, e)
+                self.logger.exception(
+                    'FBInfer was unable to compile CMake build for "%s": %s',
+                    os.getcwd(), e)
 
-        # Build the infer command to execute.
-        infer_cmd = [
-            'infer',
-            'run',
-            '--',
-        ]
+                review.general_comment(
+                    'FBInfer was unable to build this project using CMake.',
+                    rich_text=True)
 
-        # Append the build type.
-        infer_cmd += shlex.split(self.build_type)
+                return
 
-        # Check for a specified build target.
-        if settings['build_target'] != '':
-            if self.build_type == 'xcodebuild':
-                infer_cmd.append('-target')
-            infer_cmd.append(settings['build_target'])
+        if build_type in self.MULTI_FILE_BUILD_TYPES:
+            try:
+                execute(base_command,
+                        ignore_errors=True,
+                        return_errors=True)
+            except Exception as e:
+                self.logger.exception(
+                    'FBInfer was unable to build "%s" using %s: %s',
+                    os.getcwd(), build_type, e)
 
-        # Append necessary configurations and SDKs for Xcode.
-        if settings['xcode_configuration'] != '':
-            infer_cmd += [
-                '-configuration',
-                settings['xcode_configuration']
-            ]
+                review.general_comment(
+                    'FBInfer was unable to build this project:\n',
+                    rich_text=True)
 
-        if settings['sdk'] != '':
-            infer_cmd += [
-                '-sdk',
-                settings['sdk']
-            ]
+                return
 
-        try:
-            execute(infer_cmd)
-        except Exception as e:
-            logger.exception('FBInfer run command failed with build type: '
-                             '%s %s', self.build_type, e)
+            report = self._load_report([
+                _file.patched_file_path
+                for _file in files
+            ])
+        else:
+            report = []
 
-    def run_single_file_build(self, f):
-        """Perform execution of FBInfer run on a single file
+            super(FBInferTool, self).handle_files(files=files,
+                                                  base_command=base_command,
+                                                  report=report,
+                                                  review=review,
+                                                  **kwargs)
+
+        cwd = os.getcwd()
+        paths_to_files = {
+            os.path.relpath(_file.patched_file_path, cwd): _file
+            for _file in files
+        }
+
+        for entry in sorted(report,
+                            key=lambda _entry: (_entry['file'],
+                                                _entry['line'],
+                                                _entry['column'])):
+            # We should be able to trust that this file is in the map above,
+            # since we've previously filtered reports by eligible file paths.
+            f = paths_to_files[entry['file']]
+
+            column = entry['column']
+
+            if column == -1:
+                # The error pertains to the whole line.
+                column = None
+
+            f.comment(text=entry['qualifier'],
+                      severity=entry['severity'],
+                      error_code=entry['bug_type_hum'],
+                      first_line=entry['line'],
+                      start_column=column,
+                      rich_text=True)
+
+    def handle_file(self, f, path, review, base_command, report, **kwargs):
+        """Perform a review of a single file.
+
+        If this is part of a multi-file build, the results for this file from
+        that build will be reported. If not a multi-file build, then
+        :command:`infer` will be run on this file before reporting results.
 
         Args:
             f (reviewbot.processing.review.File):
                 The file to process.
-        """
-        path = f.get_patched_file_path()
 
-        if not path:
+            path (unicode):
+                The local path to the patched file to review.
+
+            review (reviewbot.processing.review.Review):
+                The review being populated.
+
+            base_command (list of unicode):
+                The base command used to run infer.
+
+            report (list of dict):
+                The report to append to.
+
+            **kwargs (dict, unused):
+                Additional keyword arguments.
+        """
+        try:
+            execute(base_command + [path],
+                    with_errors=True,
+                    ignore_errors=True)
+        except Exception as e:
+            self.logger.exception(
+                'FBInfer run command failed to build file "%s" using '
+                '"%s": %s',
+                path, base_command, e)
+
+            review.general_comment(
+                'FBInfer was unable to build `%s`.' % path,
+                rich_text=True)
+
             return
 
+        report += self._load_report([path])
+
+    def _load_report(self, paths):
+        """Return report entries for the provided paths.
+
+        This will load the JSON report from FBInfer and filter it down to any
+        results that pertain to the provided list of paths.
+
+        Args:
+            paths (list of unicode):
+                The list of absolute paths to files used to filter down
+                the report.
+
+        Returns:
+            list of dict:
+            The filtered report entries.
+        """
+        # The report will have paths relative to the root of the tree, so
+        # we'll need to normalize our paths so we can compare them.
+        cwd = os.getcwd()
+        paths = {
+            os.path.relpath(_path, cwd)
+            for _path in paths
+        }
+
+        # FullRepositoryToolMixin will put us in the right directory for
+        # this file. We can generate an absolute path from there.
+        report_filename = os.path.join(cwd, 'infer-out', 'report.json')
+
         try:
-            execute(['infer', 'run', '--'] +
-                    shlex.split(self.build_type) +
-                    [path])
+            with open(report_filename, 'r') as fp:
+                return [
+                    _entry
+                    for _entry in json.load(fp)
+                    if _entry['file'] in paths
+                ]
         except Exception as e:
-            logger.exception('FBInfer run command failed with file: '
-                             '%s %s', path, e)
+            self.logger.exception('Infer report file (%s) could not be '
+                                  'loaded: %s',
+                                  report_filename, e)
+            return []
--- docs/reviewbot/tools/fbinfer.rst
+++ docs/reviewbot/tools/fbinfer.rst
@@ -4,11 +4,12 @@
 FBInfer
 =======
 
-`FBInfer`_ is a static analysis tool that can compile a wide range of projects
-and produce a list of potential bugs. These bugs can then be intercepted
-before they have shipped to users, preventing crashes and poor performance in
-production. The types of projects that can be used with FBInfer include but
-are not limited to:
+.. versionadded:: 3.0
+
+FBInfer_ is a static analysis tool compatible with a large number of
+languages, and offering a wide variety of checks.
+
+The following types of projects can be built using FBInfer:
 
 * Android
 * Apache Ant
@@ -23,33 +24,136 @@ are not limited to:
 * Objective-C
 * XCode
 
+Unlike many tools, this doesn't operate on individual files.
+
+
 .. _FBInfer: https://fbinfer.com/
 
 
 Installation
 ============
 
-This tool requires that FBInfer is installed on the system running the Review
-Bot worker. FBInfer is available through Homebrew (Mac only), as a binary
-release (for Linux or Mac users that do not wish to use Homebrew) and
-alternatively as a Docker image. More information about `installation`_ can be
-found on the official FBInfer website.
+This tool requires FBInfer to be installed on the system running the Review
+Bot worker. This is a large dependency, so make sure you have sufficient
+storage and RAM available.
 
-.. _installation: https://fbinfer.com/docs/getting-started/
+See the `FBInfer installation guide`_ for installation instructions.
+
+.. note::
+
+   We **do not recommend** using the FBInfer Docker image with Review Bot.
+   FBInfer will be run any time a change is posted or updated, and the
+   Docker image is not suitable for this use case.
+
+
+.. _FBInfer installation guide: https://fbinfer.com/docs/getting-started/
 
 
 Configuration
 =============
 
+FBInfer Location
+----------------
+
+Review Bot will try looking for the :command:`infer` command in Review Bot's
+:envvar:`PATH` environment variable. If it can't be found, or a different name
+is used for this tool (such as :command:`run.sh`), then you'll need to specify
+the path in the :ref:`Review Bot worker config file <worker-configuration>`:
+
+.. code-block:: python
+
+    exe_paths = {
+        'infer': '/path/to/infer',
+    }
+
+You will need to restart the Review Bot worker after making this change.
+
+
+Enabling Full Repository Access
+-------------------------------
+
 This tool requires full repository access, which is available for Git and
 Mercurial repositories. Each repository you intend to use must be configured
-in the Review Bot worker config file. See :ref:`worker-configuration` for more
-details.
+in the Review Bot worker config file.
+
+See :ref:`worker-configuration-repositories` for instructions.
+
+
+Preparing Your Build Environment
+--------------------------------
 
 Because FBInfer can run static analysis on a wide variety of projects,
 the source code will often require numerous external dependencies or
-compile-time flags. The tool configuration allows you to specify additional
-command line arguments that will be passed to :command:`infer`. It's
-recommended that you set up the worker on a system which is already set up
-to build your software in order to ensure that the necessary build environment
-is available.
+compile-time flags.
+
+Compile-time flags can be specified in the worker configuration (documented
+below), but you will need to install any dependencies on the worker.
+
+It's recommended that you set up the worker on a system which is already set
+up to build your software in order to ensure that the necessary build
+environment is available.
+
+
+Enabling FBInfer in Review Board
+--------------------------------
+
+First, you'll need to add a Review Bot configuration in Review Board (see
+:ref:`extension-configuration-tools`).
+
+The following configuration options are available:
+
+:guilabel:`Build system` (required):
+    The build system used to compile the project. The following build system
+    options are supported:
+
+    :guilabel:`Android/Gradle with Wrapper`:
+        Runs :command:`infer run -- ./gradlew`
+
+    :guilabel:`Apache Ant`:
+        Runs :command:`infer run -- ant`
+
+    :guilabel:`Buck`
+        Runs :command:`infer run -- buck build`
+
+    :guilabel:`Clang (C/Objective-C)`
+        Runs :command:`infer run -- clang -c`
+
+    :guilabel:`CMake`
+        Runs :command:`infer run -- cmake`
+
+    :guilabel:`Gradle`
+        Runs :command:`infer run -- gradle`
+
+    :guilabel:`Java`
+        Runs :command:`infer run -- javac`
+
+    :guilabel:`Make`
+        Runs :command:`infer run -- make`
+
+    :guilabel:`Maven`
+        Runs :command:`infer run -- mvn`
+
+    :guilabel:`XCode`
+        Runs :command:`infer run -- xcodebuild`
+
+:guilabel:`Build target` (optional):
+    The name of the target to build, if the build system needs one or is
+    capable of building multiple targets.
+
+    For XCode, this will use :command:`xcodebuild -target <name>`. For all
+    other build systems, the target will be added after the build system
+    command above.
+
+:guilabel:`XCode configuration` (optional):
+    Any additional configuration options needed for the XCode build.
+
+    This is ignored for non-XCode builds.
+
+    This is equivalent to :command:`xcodebuild -configuration <configuration>`.
+
+:guilabel:`XCode SDK` (optional):
+    The name of an SDK to include for XCode configurations.
+
+    This is ignored for non-XCode builds.
+
+    This is equivalent to :command:`xcodebuild -sdk <sdk>`.
