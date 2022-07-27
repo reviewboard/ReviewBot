d60c3336eeecf4db523b7c3e0275e7b53096ee92
Christian Hammond <christian@beanbaginc.com>
2022-07-26 21:29:34 -0700

    Bump to 3.1.2 dev.

--- bot/reviewbot/__init__.py
+++ bot/reviewbot/__init__.py
@@ -5,7 +5,7 @@
 #:
 #: (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #:
-VERSION = (3, 1, 1, 0, 'final', 0, True)
+VERSION = (3, 1, 2, 0, 'alpha', 0, False)
 
 
 def get_version_string():
