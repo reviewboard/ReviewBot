deaa803adbeabc303890a363da717155abd7b82f
Christian Hammond <christian@beanbaginc.com>
2023-05-30 20:44:38 -0700

    Cap the kombu and RBTools dependencies on Python < 3.8.
    Kombu is dropping Python 3.7 support in their upcoming 5.3 releases,
    which are available on PyPI right now as pre-releases. When installing
    Review Bot via eggs, `easy_install` doesn't know to avoid pre-releases
    (or to consider supported Python version metadata), so we have to cap
    this until we drop support for Python 2.7 and can depend fully on
    wheels.
    
    RBTools dropped Python 2.7 and 3.6 in RBTools 4. We had a pretty old
    minimum version, so I've taken the opportunity to bump that up. We now
    depend on RBTools 3.x for Python 2.7/3.7, and RBTools 4.x or higher for
    newer releases.
    
    Testing Done:
    Successfully installed into new Python 2.7, 3.7, and 3.8 environments.
    
    Reviewed at https://reviews.reviewboard.org/r/13081/

--- bot/setup.py
+++ bot/setup.py
@@ -57,10 +57,16 @@ setup(
         # We have to cap kombu for Python 3.6, as celery 5.1.x covers too
         # broad a range of versions, resulting in a Python 3.6-incompatible
         # kombu to be installed.
+        #
+        # Kombu 5.3 also drops Python 3.7 support.
         'kombu>=5.1.0,<=5.1.999; python_version == "3.6"',
+        'kombu>=5.1.0,<=5.2.999; python_version == "3.7"',
 
         'enum; python_version == "2.7"',
-        'RBTools>=1.0',
+
+        # RBTools 4.0 drops Python 2.7 and 3.6.
+        'RBTools>=3.0,<=3.999; python_version < "3.7"',
+        'RBTools>=4.0; python_version >= "3.7"',
 
         # We unconditionally include these plugins to ensure they're present.
         'flake8-json>=21.1',
